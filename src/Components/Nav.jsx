abb661b6c532ababaab85d8641eaeb4e356716d1
Kuldeepkant26 <picasuplayers@gmail.com>
2024-11-08 01:38:49 +0530

    improved home button conditional availability

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-
-import { ToastContainer, toast } from 'react-toastify';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import '../css/nav.css';
@@ -10,67 +9,91 @@ import ProfileEdit from './ProfileEdit';
 import UserInfoEdit from './UserInfoEdit';
 
 function Nav() {
-    const { counter, currUser, logout } = useContext(MyContext)
+    const { currUser, logout } = useContext(MyContext);
     const navigate = useNavigate();
+    const location = useLocation();
     const [menuOpen, setMenuOpen] = useState(false);
+
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
+
     const closeMenu = () => {
         setMenuOpen(false);
-    }
+    };
+
     function handelLogout() {
         logout();
     }
+
     return (
         <>
             <nav>
-                <h3>GYANBOT </h3>
+                <h3>GYANBOT</h3>
                 <div className={`menu ${menuOpen ? 'slidemenu' : ''}`}>
                     <i
                         id="mnuclose"
                         className={`ri-arrow-right-line ${menuOpen ? '' : 'rotate180'}`}
                         onClick={toggleMenu}
                     ></i>
-                    <Link className="options" to={'/'} onClick={closeMenu}>
-                        Home
-                    </Link>
-                    <Link className="options" to={'/allposts'} onClick={closeMenu}>
+
+                    {/* Conditionally render "Home" link if not on the home route */}
+                    {location.pathname !== '/' && (
+                        <Link className="options" to="/" onClick={closeMenu}>
+                            Home
+                        </Link>
+                    )}
+
+                    <Link className="options" to="/allposts" onClick={closeMenu}>
                         Explore
                     </Link>
 
-                    {localStorage.getItem('gyanbot-auth-token') && currUser ?
+                    {localStorage.getItem('gyanbot-auth-token') && currUser ? (
                         <>
-                            <button className="options" onClick={() => { toggleMenu(), handelLogout(), navigate('/') }}>
+                            <button
+                                className="options"
+                                onClick={() => {
+                                    toggleMenu();
+                                    handelLogout();
+                                    navigate('/');
+                                }}
+                            >
                                 Logout
                             </button>
-                            <Link className="options " to={`/profile/${currUser._id}`} onClick={toggleMenu}>
-                                {currUser.profilePicture ? <img className='user-profile-pic m-auto object-cover' src={currUser.profilePicture} alt='A'></img> : <p className='profile-button'>
-                                    {currUser.name[0]}
-                                </p>}
+                            <Link
+                                className="options"
+                                to={`/profile/${currUser._id}`}
+                                onClick={toggleMenu}
+                            >
+                                {currUser.profilePicture ? (
+                                    <img
+                                        className="user-profile-pic m-auto object-cover"
+                                        src={currUser.profilePicture}
+                                        alt="Profile"
+                                    />
+                                ) : (
+                                    <p className="profile-button">{currUser.name[0]}</p>
+                                )}
                             </Link>
-                        </> :
+                        </>
+                    ) : (
                         <>
-                            <Link className="options" to={'/signup'} onClick={toggleMenu}>
+                            <Link className="options" to="/signup" onClick={toggleMenu}>
                                 Signup
                             </Link>
-                            <Link className="options" to={'/login'} onClick={toggleMenu}>
+                            <Link className="options" to="/login" onClick={toggleMenu}>
                                 Login
                             </Link>
-                        </>}
+                        </>
+                    )}
                 </div>
 
                 <i id="mnubtn" className="ri-menu-line" onClick={toggleMenu}></i>
-
             </nav>
 
-            <ToastContainer
-                position="top-center"
-                autoClose={1500}
-                hideProgressBar
-            ></ToastContainer>
-            <ProfileEdit></ProfileEdit>
-            <UserInfoEdit></UserInfoEdit>
+            <ToastContainer position="top-center" autoClose={1500} hideProgressBar />
+            <ProfileEdit />
+            <UserInfoEdit />
         </>
     );
 }
