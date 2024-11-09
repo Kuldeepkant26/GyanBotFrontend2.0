cad1646188ab42fd15ba8d4b4a7f90ba5dccc01d
Kuldeepkant26 <picasuplayers@gmail.com>
2024-11-10 04:06:27 +0530

    Added notification referesh button

--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -4,7 +4,7 @@ import { MyContext } from '../Context/MyProvider';
 import NotificationCard from './NotificationCard';
 
 function Notification() {
-    const { setshowNotification, currUser, newNotification, setnewNotification } = useContext(MyContext);
+    const { setshowNotification, currUser, newNotification, setnewNotification, fetchUser } = useContext(MyContext);
 
     const [allNotifications, setAllNotifications] = useState([]);
 
@@ -19,10 +19,18 @@ function Notification() {
         }
     }, [currUser]); // Re-run when `currUser` changes
 
+    function handelReferesh() {
+        fetchUser(localStorage.getItem('gyanbot-auth-token'))
+        setAllNotifications(currUser.notifications);
+        setnewNotification(currUser.notifications.filter((noti) => noti.issNew))
+    }
+
     return (
         <div className='notification'>
+
             <div className="top">
                 <p>Notifications {newNotification.length ? <span className='text-red-500 text-[14px]'>({newNotification.length} new) </span> : <></>}</p>
+                <div className='referesh-btn' onClick={handelReferesh}>referesh<i class="ri-loop-right-line"></i></div>
                 <i className="ri-close-large-fill" onClick={() => setshowNotification(false)}></i>
             </div>
             <div className="bottom">
@@ -36,7 +44,7 @@ function Notification() {
                     <>
                         <i className="ri-notification-2-line"></i>
                         <h3>Your notifications live here</h3>
-                        
+
                     </>
                 )}
             </div>
